dc95da7126f16547d5d4eb34b71c7c194fdf95e6
María de la Almudena Martín Gómez <147110337+almudenamartinn@users.noreply.github.com>
2025-01-04 17:30:41 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+from functions import (
+    obtain_data_position,
+    calculate_distance,
+    apply_corrections,
+    check_significant_change
+)
+
+if __name__ == "__main__":
+    from functions import main
+    main()
+
